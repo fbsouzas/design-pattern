d0a9bb044c4b927d0206c89fbe26e2efbb590bea
Fábio Souza <fabio.souza@vaipe.com.br>
2021-05-10 21:22:32 -0300

    feat: adds state and calculate extra discount to a budget

--- src/Budgets/Budget.php
+++ src/Budgets/Budget.php
@@ -4,8 +4,32 @@ declare(strict_types=1);
 
 namespace Fbsouzas\DesignPattern\Budgets;
 
+use DomainException;
+
 class Budget
 {
     public int $quantityOfItems;
     public float $value;
+    public string $state;
+
+    public function applyExtraDiscount(): void
+    {
+        $this->value -= $this->calculateExtraDiscount();
+    }
+
+    /**
+     * @throws DomainException
+     */
+    private function calculateExtraDiscount(): float
+    {
+        if ($this->state == 'IN_APPROVAL') {
+            return $this->value * 0.05;
+        }
+
+        if ($this->state == 'APPROVED') {
+            return $this->value * 0.02;
+        }
+
+        throw new DomainException('A budget disapproved or finished can not received an extra discount.');
+    }
 }
