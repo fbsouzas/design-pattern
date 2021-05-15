edbd1896e31341541b8865503abebccc2169502a
Fábio Souza <fabio.souza@vaipe.com.br>
2021-05-15 13:43:00 -0300

    feat: adds orders list cli

---
+++ orders-cli.php
@@ -0,0 +1,34 @@
+<?php
+
+use Fbsouzas\DesignPattern\Budgets\Budget;
+
+require 'vendor/autoload.php';
+
+$budget1 = new Budget();
+$budget1->quantityOfItems = 7;
+$budget1->value = 700;
+$budget1->approve();
+
+$budget2 = new Budget();
+$budget2->quantityOfItems = 15;
+$budget2->value = 1500.52;
+$budget2->approve();
+$budget2->finish();
+
+$budget3 = new Budget();
+$budget3->quantityOfItems = 3;
+$budget3->value = 50;
+$budget3->disapprove();
+
+$budgetList = [
+    $budget1,
+    $budget2,
+    $budget3,
+];
+
+foreach ($budgetList as $budget) {
+    echo 'Value: ' . $budget->value . PHP_EOL;
+    echo 'Items quantity: ' . $budget->quantityOfItems . PHP_EOL;
+    echo 'Stastus: ' . get_class($budget->state) . PHP_EOL;
+    echo PHP_EOL;
+}
