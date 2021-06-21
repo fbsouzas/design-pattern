a64d7aab8082d5c63c859294121383cc0b6a3f59
Fábio Souza <fabio.souza@vaipe.com.br>
2021-06-21 16:40:18 -0300

    refactor: implements memory optimization

--- thousands-orders-cli.php
+++ thousands-orders-cli.php
@@ -6,12 +6,13 @@ use Fbsouzas\DesignPattern\Orders\Order;
 require 'vendor/autoload.php';
 
 $orders = [];
+$currentDate = new DateTimeImmutable();
 
 for ($i = 0; $i < 100000; $i++) {
     $order = new Order();
 
     $order->clientName = md5('a');
-    $order->finishDate = new DateTimeImmutable();
+    $order->finishDate = $currentDate;
     $order->budget = new Budget();
 
 
